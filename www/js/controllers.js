cf4249258d3513c73e4b79b3f17a9360106b427c
raniaaa <rania.wannes@gmail.com>
2015-10-26 16:39:34 +0100

    “msg2”

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$state) {
+.controller('DashCtrl', function($scope,$state, events) {
   $scope.Go = function(){
+console.log(events.all());
 
     $state.go("home");
   }
 })
 
-.controller('HomeCtrl', function($scope, Chats) {
+.controller('HomeCtrl', function($scope,events,$timeout) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,7 +16,20 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $scope.firstColor = "#000000";
+  var app1 = events.all();
+console.log(app1);
+if(app1.Type == 'Color'){
 
+  $scope.firstColor= '#'+app1.content;
+  $timeout(function(){
+    $scope.firstColor = "#FF0000";
+    $timeout(function(){
+      $scope.firstColor = "#008000";
+
+    },2000);
+  },2000);
+}
 
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,29 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('events', function() {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+
+  var events =
+    { "id":1,
+    "start_time": "2010-01-01T04:04:00+01:00",
+    "end_time": "2010-01-01T04:05:00+01:00",
+     "Type": "Color",
+     "content": "ffee55",
+     "comment": "."};
+
 
   return {
     all: function() {
-      return chats;
+      return events;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(event) {
+      events.splice(events.indexOf(event), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(id) {
+      for (var i = 0; i < events.length; i++) {
+        if (events[i].id === parseInt(id)) {
+          return events[i];
         }
       }
       return null;
